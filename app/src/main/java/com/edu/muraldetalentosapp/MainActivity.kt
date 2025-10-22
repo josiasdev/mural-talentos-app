4b59daaa66173892f48eec9d6ed5096e5c3cf180
CristianoMends <mendescristiano012@gmail.com>
2025-10-22 19:58:15 -0300

    feat: criado códio base

---
+++ app/src/main/java/com/edu/muraldetalentosapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.edu.muraldetalentosapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.edu.muraldetalentosapp.ui.theme.MuralDeTalentosAPPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MuralDeTalentosAPPTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MuralDeTalentosAPPTheme {
+        Greeting("Android")
+    }
+}
